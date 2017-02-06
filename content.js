f16ecac1d204eafc1e90ff6749bb2211dfa0a21a
Daniel Gutierrez <daniel.gutierrez@degt.cl>
2017-02-06 17:26:55 -0300

    search added

--- content.js
+++ content.js
@@ -3,30 +3,47 @@
 (function() {
 
     var Def = function() {
-        return constructor.apply(this, arguments); 
+        return constructor.apply(this, arguments);
     }
 
     function constructor() {
 
         var plugin = this;
         var modyoVersion = $('meta[name=modyo-version]').attr("content");
-        
-        if(!modyoVersion){
+
+        if (!modyoVersion) {
             return false;
         }
 
         console.log('ModyoSuperpowers', 'init');
 
-        plugin.makeBetterSnippets();
+        //ToDo: Optimize calls
+        // $("html").bind("DOMSubtreeModified", function() {
+        //     plugin.makeBetterSnippets();
+        // });
 
 
-        // CMD+S or CTRL+S
+        $(document).on('click', "a[href='#snippets']", function() {
+            console.log("test");
+            plugin.makeBetterSnippets();
+        });
+
+
         $(document).keydown(function(event) {
+
+            //Ctrl+w
+            if ((event.ctrlKey || event.metaKey) && event.which == 87) {
+                plugin.closeTab();
+                event.preventDefault();
+                return false;
+            }
+
+            // CMD+S or CTRL+S
             // If Control or Command key is pressed and the S key is pressed
             // run save function. 83 is the key code for S.
             if ((event.ctrlKey || event.metaKey) && event.which == 83) {
                 // Save Function
-                plugin.dummySave();
+                plugin.saveAll();
                 event.preventDefault();
                 return false;
             };
@@ -35,7 +52,31 @@
 
 
     makeBetterSnippets = function() {
-        console.log('makeBetterSnippets');
+        var plugin = this;
+
+        if (!$('#snippet-list-container') || $('#snippet-list-container snippet-item').hasClass('superpowers')) {
+            return;
+        } else {
+            $('#snippet-list-container snippet-item').addClass('superpowers');
+        }
+
+
+        // //Change snippets order
+        //$("#snippets").append($('#snippets .accordion'));
+
+        //Close accordion
+        $("#snippets .show-hide:contains('[-]')").click();
+
+        //Search
+        if($('#snippets-search').length == 0){
+            $('#snippet-list-container').prepend("<input type='text' id='snippets-search' placeholder='Buscar Snippet' class='span-col-half search'> ");
+            $('#snippets-search').keyup(function(){
+                console.log('Its alive');
+                plugin.findSnippet();
+            });
+        }
+        
+
         $('#snippet-list-container').find('.snippet-item').each(function(i, e) {
 
             var item = $(e).find("a").first();
@@ -44,8 +85,6 @@
 
             //Add color 
             icon.removeClass("icon-file-alt");
-
-            console.log(item.attr('href'));
 
             switch (data[0]) {
                 case 'style':
@@ -72,16 +111,39 @@
                     icon.addClass("icon-file-alt");
             }
 
+
+
         });
     }
 
-    dummySave = function() {
-        //Experimental
+    saveAll = function() {
         $(".form-actions button:last-child").click();
     }
 
-    findSnippet = function(){
-        
+    closeTab = function() {
+        console.log("Close tab");
+        $(".header-tabs .active a")[0].click();
+    }
+
+    findSnippet = function() {
+
+        // Declare variables
+        var input, filter, ul, li, a, i;
+        input = document.getElementById('snippets-search');
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("snippet-list-container");
+        li = ul.getElementsByTagName('div');
+
+        // Loop through all list items, and hide those who don't match the search query
+        for (i = 0; i < li.length; i++) {
+            a = li[i].getElementsByTagName("a")[0];
+            if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+
     }
 
 
